8f5cd1e8988f599f1a899093cde73cc607196f7d
AndreasLc1103 <Andreaslc1103@gmail.com>
2022-09-18 17:08:55 -0400

    finalized stubbed out function

---
+++ Week4/src/stairs.py
@@ -0,0 +1,31 @@
+""" stairs.py is a file responsible for being the main runner for the stairs challenge.
+    Students will be able to run this file against another suite of test case to see 
+    if they had successfully finished the challenge. the challenge problem is here 
+
+There's a staircase with N steps,
+and you can climb 1 or 2 steps at a time.
+Given N, 
+write a function that returns the number of unique
+ways you can climb the staircase. The order of the steps matters.
+For example, if N is 4, then there are 5 unique ways:
+
+    1, 1, 1, 1
+    2, 1, 1
+    1, 2, 1
+    1, 1, 2
+    2, 2
+
+What if, 
+instead of being able to climb 1 or 2 steps at a time, 
+you could climb any number from a set of positive integers X?
+For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time. 
+Generalize your function to take in X. 
+"""
+
+
+def stairs(N: int):
+    """stairs is a function responsible for testing students code for the
+    stairs challenge.
+        N (int): defining the desired target value to calculate stairs.
+    """
+    pass
